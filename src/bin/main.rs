e4bb0db5f953b233b7c2950968f17169bafc70e1
Konstantin Đorđević <konstantin.djordjevic@tradecore.com>
2018-12-26 10:38:31 +0100

    Update main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,48 +1,33 @@
 extern crate influx_db_client;
 extern crate market_data_time_series;
 
-use influx_db_client::{Client, Point, Points, Precision, Value};
+use influx_db_client::{Client, Point, Precision, Value};
 
-use market_data_time_series::mt5_event_subscriber::{Mt5EventSubscriber, Mt5EventZmqFrames};
-use market_data_time_series::ohlc::ohlcmap::OhlcMap;
-use market_data_time_series::protobuf_tick::named_tick::NamedTick;
-
-fn zmq2tick(tick: Mt5EventZmqFrames) -> NamedTick {
-    NamedTick {
-        symbol: String::from_utf8(tick.name).unwrap(),
-        tick: NamedTick::parse_tick_from_bytes(&tick.payload),
-    }
-}
+use market_data_time_series::mt5_event_subscriber::Mt5EventSubscriber;
+use market_data_time_series::ohlc::Ohlc;
+use market_data_time_series::named_tick::NamedTick;
 
 fn main() {
-    let client =
-        Client::new("http://192.168.56.1:8086", "mydb").set_authentication("mt5feed", "mt5feed");
-
     let ctx = market_data_time_series::zmq::Context::new();
     let subscriber = Mt5EventSubscriber::new(ctx, b"MDT", "tcp://mt5proxy.tradecore.io:8149");
-    let mut ohlc_map = OhlcMap::new();
+    let client = Client::new("http://influxdb:8086", "mdts").set_authentication("mt5", "mt5");
 
-    for tick in subscriber.into_iter().map(zmq2tick) {
-        if ohlc_map.is_next_min(&tick) {
-            let mut measurements = Points { point: Vec::new() };
-            for (symbol, ohlc) in &ohlc_map.map {
-                let timestamp = ohlc_map.min * 60;
-                let measurement = Point::new(symbol)
-                    .add_field("open", Value::Float(ohlc.open))
-                    .add_field("high", Value::Float(ohlc.high))
-                    .add_field("low", Value::Float(ohlc.low))
-                    .add_field("close", Value::Float(ohlc.close))
-                    .add_field("volume", Value::Integer(ohlc.volume as i64))
-                    .add_timestamp(timestamp)
-                    .to_owned();
-                measurements.push(measurement);
-                println!("{} : {:#?}", symbol, ohlc);
-            }
-            let _ = client
-                .write_points(measurements, Some(Precision::Seconds), None)
-                .unwrap();
-            println!("***************{}**********************", ohlc_map.min);
+    for named_tick in subscriber.into_iter().map(NamedTick::from_frames) {
+        let ohlc = Ohlc::new(&named_tick);
+        let point = Point::new("ticks")
+            .add_tag("symbol", Value::String(named_tick.symbol))
+            .add_field("open", Value::Float(ohlc.open))
+            .add_field("high", Value::Float(ohlc.high))
+            .add_field("low", Value::Float(ohlc.low))
+            .add_field("close", Value::Float(ohlc.close))
+            .add_field("volume", Value::Integer(ohlc.volume as i64))
+            .add_timestamp(named_tick.tick.datetime)
+            .to_owned();
+        // TODO: Retention policy
+        // TODO: Clone?
+        match client.write_point(point.clone(), Some(Precision::Seconds), None) {
+            Ok(_) => println!("Wrote: {:?}", point),
+            Err(error) => println!("Error: {}", error),
         }
-        ohlc_map.update(&tick);
     }
 }
