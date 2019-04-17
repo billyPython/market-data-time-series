12c064a5eac885bf8448a0edd2d7e46318334030
Konstantin Đorđević <konstantin.djordjevic@tradecore.com>
2019-04-17 11:25:59 +0200

    Add backfill queries to ticks_cq.sql

--- ticks_cq.sql
+++ ticks_cq.sql
@@ -1,14 +1,14 @@
 USE mdts
 
--- DELETE FROM ticks_1m
--- DELETE FROM ticks_5m
--- DELETE FROM ticks_15m
--- DELETE FROM ticks_30m
--- DELETE FROM ticks_1h
--- DELETE FROM ticks_4h
--- DELETE FROM ticks_1d
--- DELETE FROM ticks_1w
--- DELETE FROM ticks_4w
+DELETE FROM ticks_1m
+DELETE FROM ticks_5m
+DELETE FROM ticks_15m
+DELETE FROM ticks_30m
+DELETE FROM ticks_1h
+DELETE FROM ticks_4h
+DELETE FROM ticks_1d
+DELETE FROM ticks_1w
+DELETE FROM ticks_4w
 
 DROP CONTINUOUS QUERY ticks_1m  ON mdts
 DROP CONTINUOUS QUERY ticks_5m  ON mdts
@@ -29,3 +29,13 @@ CREATE CONTINUOUS QUERY ticks_4h  ON mdts BEGIN SELECT first(symbol) AS symbol, 
 CREATE CONTINUOUS QUERY ticks_1d  ON mdts BEGIN SELECT first(symbol) AS symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_1d  FROM ticks GROUP BY time(1d),  symbol END
 CREATE CONTINUOUS QUERY ticks_1w  ON mdts BEGIN SELECT first(symbol) AS symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_1w  FROM ticks GROUP BY time(1w),  symbol END
 CREATE CONTINUOUS QUERY ticks_4w  ON mdts BEGIN SELECT first(symbol) AS symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_4w  FROM ticks GROUP BY time(4w),  symbol END
+
+SELECT first(symbol) as symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_4w  FROM ticks GROUP BY time(4w),  symbol
+SELECT first(symbol) as symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_1w  FROM ticks GROUP BY time(1w),  symbol
+SELECT first(symbol) as symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_1d  FROM ticks GROUP BY time(1d),  symbol
+SELECT first(symbol) as symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_4h  FROM ticks GROUP BY time(4h),  symbol
+SELECT first(symbol) as symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_1h  FROM ticks GROUP BY time(1h),  symbol
+SELECT first(symbol) as symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_30m FROM ticks GROUP BY time(30m), symbol
+SELECT first(symbol) as symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_15m FROM ticks GROUP BY time(15m), symbol
+SELECT first(symbol) as symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_5m  FROM ticks GROUP BY time(5m),  symbol
+SELECT first(symbol) as symbol, first(open) AS open, max(high) AS high, min(low) AS low, last(close) AS close, sum(volume) AS volume, count(volume) AS tick_volume INTO ticks_1m  FROM ticks GROUP BY time(1m),  symbol
